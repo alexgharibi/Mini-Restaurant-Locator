c60e0245c6d32ba5f660464bf53f2f09e2d40b0c
alexgharibi <ali52346@gmail.com>
2022-01-31 10:09:41 -0500

    Handle error

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
 import { View, Text, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState("");
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: term,
+          location: "ottawa",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setError("somthing went wrong");
+    }
+  };
+
   return (
     <View>
       <SearchBar
@@ -11,10 +30,10 @@ const SearchScreen = () => {
         onTermChange={(newTerm) => {
           setTerm(newTerm);
         }}
-        onTermSubmit={() => console.log("hi")}
+        onTermSubmit={searchApi}
       />
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      {error ? <Text>{error}</Text> : null}
+      <Text>{results.length}</Text>
     </View>
   );
 };
