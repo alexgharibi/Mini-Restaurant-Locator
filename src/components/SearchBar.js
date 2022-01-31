2690fc7142320e7ff227f84fb6a47479af179934
alexgharibi <ali52346@gmail.com>
2022-01-31 08:41:35 -0500

    Add css to searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,7 @@ import { Feather } from "@expo/vector-icons";
 const SearchBar = () => {
   return (
     <View style={styles.backgroundStyle}>
-      <Feather name="search" size={30} />
+      <Feather name="search" style={styles.iconStyle} />
       <TextInput placeholder="Search" style={styles.inputStyle} />
     </View>
   );
@@ -18,12 +18,18 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginHorizontal: 15,
     flexDirection: "row",
+    marginTop: 15,
   },
 
   inputStyle: {
-    borderWidth: 1,
-    borderColor: "black",
     flex: 1,
+    fontSize: 18,
+  },
+
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
   },
 });
 
