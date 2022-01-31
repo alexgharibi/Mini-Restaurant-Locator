964af7f1b3e4101f71b25c58f1e17c6563de3da6
alexgharibi <ali52346@gmail.com>
2022-01-31 08:50:36 -0500

    Add state to textInput

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
 import { View, Text, StyleSheet } from "react-native";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
   return (
     <View>
-      <SearchBar />
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => {
+          setTerm(newTerm);
+        }}
+      />
       <Text>Search Screen</Text>
+      <Text>{term}</Text>
     </View>
   );
 };
