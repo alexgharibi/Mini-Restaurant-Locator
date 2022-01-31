9a949648e99cf4a06ef8e195033c39fc94640675
alexgharibi <ali52346@gmail.com>
2022-01-31 08:37:16 -0500

    Add textinput

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" size={30} />
+      <TextInput placeholder="Search" style={styles.inputStyle} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: "#F0EEEE",
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
+    flexDirection: "row",
+  },
+
+  inputStyle: {
+    borderWidth: 1,
+    borderColor: "black",
+    flex: 1,
   },
 });
 
